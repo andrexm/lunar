135da53bf0ded2dfe27fcdffcb5a0294d8102eee
André Nascimento <andre.neospace@gmail.com>
2023-08-03 17:29:09 -0300

    fixed: element loads if starts visible on screen

--- src/js/LnLazy.js
+++ src/js/LnLazy.js
@@ -1,40 +1,54 @@
 export class LnLazy extends HTMLElement {
-	#loaded = false;
+	#loaded;
+    #pos;
 
 	constructor() {
 		super();
-		this.start();
         this.style.paddingTop = "400px";
+        this.#loaded = false;
+        this.#pos = this.getBoundingClientRect().top + window.scrollY;
+        this.start(window.innerHeight >= this.getBoundingClientRect().top);
 	}
+
+    /**
+     * @param {boolean} visible element is visible when page loads
+     * @returns boolean
+     */
+    #render(visible) {
+        if (this.#loaded) return false;
+        
+        if (window.scrollY + window.innerHeight + Number(this.getAttribute('dis') ?? 400) >= this.#pos || visible) {
+            let src = this.getAttribute('src');
+            lunar.ajax({
+                method: 'GET',
+                url: src,
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+                success: data => {
+                    this.innerHTML = data;
+                    this.#loaded = true;
+                },
+                error: () => {
+                    console.error('The component ' + src + ' cannot be #loaded.');
+                    this.#loaded = true;
+                }
+            }).send();
+            this.style.paddingTop = "0px";
+        }
+        return false;
+    }
 
 	/**
 	 * Loads the content
 	 */
-	start() {
-		let pos = this.getBoundingClientRect().top + window.scrollY;
-
+	start(visible) {
+        if (visible) {
+            this.#render(visible);
+            return false;
+        }
 		document.addEventListener('scroll', () => {
-            if (this.#loaded) return false;
-
-            if (window.scrollY + window.innerHeight + Number(this.getAttribute('dis') ?? 400) >= pos) {
-                let src = this.getAttribute('src');
-                lunar.ajax({
-                    method: 'GET',
-                    url: src,
-                    headers: {
-                        'X-Requested-With': 'XMLHttpRequest'
-                    },
-                    success: data => {
-                        this.innerHTML = data;
-                        this.#loaded = true;
-                    },
-                    error: () => {
-                        console.error('The component ' + src + ' cannot be loaded.');
-                        this.#loaded = true;
-                    }
-                }).send();
-                this.style.paddingTop = "0px";
-            }
+            this.#render(visible);
         });
 	}
 }
